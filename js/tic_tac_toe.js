db37e8f1d65a218cdacd083557a6c459652feef1
Hogue <ajhogue6@gmail.com>
2015-05-11 16:16:01 -0400

    placing x and o is working

--- js/tic_tac_toe.js
+++ js/tic_tac_toe.js
@@ -1,2 +1,17 @@
-// Your code here
-// Use the jQuery
+$(document).ready(function() {
+
+  var turnCount = 0;
+
+  $('.box_cell').on("click", function(event){
+    if(turnCount % 2 === 0) {
+      $(event.currentTarget).text("x");
+    }
+    else {
+      $(event.currentTarget).text("o")
+    }
+
+    turnCount++;
+  });
+
+});
+
