ce10363afdea60295edb1de753874cd1035a8229
Hogue <ajhogue6@gmail.com>
2015-05-11 23:37:14 -0400

    function to check o for winning combinations now works

--- js/tic_tac_toe.js
+++ js/tic_tac_toe.js
@@ -69,13 +69,13 @@ $(document).ready(function() {
     if(board[0][0] === 'x' && board[0][1] === 'x' && board[0][2] === 'x' || board[1][0] === 'x' && board[1][1] === 'x' && board[1][2] === 'x' || board[2][0] === 'x' && board[2][1] === 'x' && board[2][2] === 'x' || board[0][0] === 'x' && board[1][0] === 'x' && board[2][0] === 'x' || board[0][1] === 'x' && board[1][1] === 'x' && board[1][2] === 'x' || board[0][2] === 'x' && board[1][2] === 'x' && board[2][2] === 'x'|| board[0][0] === 'x' && board[1][1] === 'x' && board[2][2] === 'x' || board[0][2] === 'x' && board[1][1] === 'x' && board[2][0] === 'x'){
       alert("winner is X!");
     }
-    }
+    };
 
   var checkForWinnerO = function() {
    if(board[0][0] === 'o' && board[0][1] === 'o' && board[0][2] === 'o' || board[1][0] === 'o' && board[1][1] === 'o' && board[1][2] === 'o' || board[2][0] === 'o' && board[2][1] === 'o' && board[2][2] === 'o' || board[0][0] === 'o' && board[1][0] === 'o' && board[2][0] === 'o' || board[0][1] === 'o' && board[1][1] === 'o' && board[1][2] === 'o' || board[0][2] === 'o' && board[1][2] === 'o' && board[2][2] === 'o'|| board[0][0] === 'o' && board[1][1] === 'o' && board[2][2] === 'o' || board[0][2] === 'o' && board[1][1] === 'o' && board[2][0] === 'o'){
      alert("winner is O!");
    }
-   }
+   };
 
 });
 
